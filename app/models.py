5c95384edd4e2b1032b506b27f0f78b4b1174535
Vohsty <stevekimanthi87@gmail.com>
2019-05-21 11:39:24 +0300

    add models.py

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class Headline:
+    """
+    Headline class to define headline objects
+
+    """
+
+    def __init__(self,title,description,content,urlToImage,url,category,id):
+        self.title = title
+        self.description = description
+        self.content = content
+        self.urlToImage = urlToImage
+        self.url = url
+        self.category = category
+        self.id = id
