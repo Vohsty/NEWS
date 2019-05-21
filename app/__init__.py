7b6e686664b8aa4f1233a794b9fea13ad8048538
Vohsty <stevekimanthi87@gmail.com>
2019-05-21 11:40:47 +0300

    __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+#from config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+
+"""
+initialize application
+
+"""
+NEWS_API_KEY = 'c71bc376a4984584b138047007ed8637'
+SECRET_KEY = 'rrrrrraaaaah'
+#app= Flask(__name__,instance_relative_config= True)
+
+# Setting up configuration
+#app.config.from_object(DevConfig)
+#app.config.from_pyfile('config.py')
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap()
+
+
+#from main import views
+#from main import errors
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    #from .requests import configure_request
+    #configure_request(app)
+
+
+    return app
